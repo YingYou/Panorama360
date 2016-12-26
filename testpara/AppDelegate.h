1823949300a5a62d8b6792147a98c775116f3240
smile <505397638@qq.com>
2016-12-26 16:37:49 +0800

    Panorama360
    Panorama360—arc模式

---
+++ testpara/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testpara
+//
+//  Created by yangwei on 2016/12/26.
+//  Copyright © 2016年 yw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testpara/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testpara
+//
+//  Created by yangwei on 2016/12/26.
+//  Copyright © 2016年 yw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
